7dfb690e1da5deaa0d65a221d2c8dbcb20e4c719
John Draper <jdraper09@icloud.com>
2014-01-09 01:34:30 -0500

    Updated debug launch script

--- debug.py
+++ debug.py
@@ -1,4 +1,9 @@
 #! /usr/bin/env python
+import sys
 from ricecrew.loader import app
 
-app.run(debug=True)
+if len(sys.argv) > 1:
+    app.run(host=sys.argv[1], debug=True)
+else:
+    app.run(debug=True)
+
