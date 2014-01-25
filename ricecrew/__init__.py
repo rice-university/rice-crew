1a418ba524b7714c640749ec9e6265280185c01e
John Draper <jdraper09@icloud.com>
2014-01-24 22:51:49 -0600

    Pages now valid XHTML, served as application/xhtml+xml

--- ricecrew/__init__.py
+++ ricecrew/__init__.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, Response
 from ricecrew import settings
+
+class XhtmlResponse(Response):
+    default_mimetype = 'application/xhtml+xml'
 
 app = Flask(__name__)
 app.config.from_object(settings)
+app.response_class = XhtmlResponse
